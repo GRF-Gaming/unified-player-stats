0ce6095ebb74c366cd4dea1567a5b01609e7c49f
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-06-03 17:40:40 +0800

    Fixed serving port and address of agg

--- backend/internal/aggregator/serve.go
+++ backend/internal/aggregator/serve.go
@@ -1,12 +1,13 @@
 package aggregator
 
 import (
+	"fmt"
 	"github.com/cloudwego/hertz/pkg/app/server"
 )
 
 func Serve(port int) {
 
-	h := server.Default(server.WithHostPorts("localhost:8080"))
+	h := server.Default(server.WithHostPorts(fmt.Sprintf("0.0.0.0:%d", port)))
 
 	h.POST("/events", handleEvents)
 
