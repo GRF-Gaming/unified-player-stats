1f56314f936709beeb0eb172e996f51d6662fff8
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-06-03 17:40:06 +0800

    Updated event schema
    - Removed API key from request (not needed as of now)
    + Added method to extract Kill records from event
    + Updated schema for EventKill to include time
    + Added EventKillRecord model for internal tracking of kills (to be used in redis/dbs)

--- backend/internal/models/event.go
+++ backend/internal/models/event.go
@@ -1,8 +1,33 @@
 package models
 
-type Event struct {
+type EventFromGame struct {
+	// ApiKey Reforger does not permit setting of headers
 	// ApiKey ApiKeyV1    `json:"api_key,required" vd:"len($)>0"`
-	ServerName  string      `json:"server_name,required" vd:"len($)>0"`
-	ServerGroup string      `json:"server_group,required" vd:"len($)>0"`
-	Kills       []EventKill `json:"kills,required" vd:"len($)>0"`
+
+	// ServerName Name of the server event(s) were observed from
+	ServerName string `json:"server_name,required" vd:"len($)>0"`
+
+	// GameName Name of the game used for aggregation by player and game
+	GameName string `json:"game_name,required" vd:"len($)>0"`
+
+	// ServerGroup Used for logical grouping for further analysis
+	//ServerGroup string              `json:"server_group,required" vd:"len($)>0"`
+
+	// Kills Recorded kill events on the server
+	Kills []EventKillFromGame `json:"kills,required" vd:"len($)>0"`
+}
+
+func (e *EventFromGame) ExtractKillRecords() []*EventKillRecord {
+
+	var allKills []*EventKillRecord
+
+	for _, k := range e.Kills {
+		allKills = append(allKills, &EventKillRecord{
+			EventKillFromGame: k,
+			ServerName:        e.ServerName,
+			GameName:          e.GameName,
+		})
+	}
+
+	return allKills
 }
--- backend/internal/models/event_kill.go
+++ backend/internal/models/event_kill.go
@@ -1,8 +1,18 @@
 package models
 
-type EventKill struct {
-	PlayerId       string `json:"player_id,required" vd:"len($)>0"`
-	PlayerName     string `json:"player_name,required" vd:"len($)>0"`
-	KilledEntityId string `json:"killed_entity_id"`
-	IsFriendly     bool   `json:"is_friendly,required"`
+import "time"
+
+type EventKillFromGame struct {
+	PlayerId       string    `json:"player_id,required" vd:"len($)>0"`   // PlayerId is the UID of the player designated by the server
+	PlayerName     string    `json:"player_name,required" vd:"len($)>0"` // PlayerName is the in-game name of the player
+	KilledEntityId string    `json:"killed_entity_id"`                   // KilledEntityId optional, only specifies the killed PLAYER
+	IsFriendly     bool      `json:"is_friendly,required"`               // IsFriendly specifies if the kill was a friendly kill
+	Time           time.Time `json:"time,required"`                      // Time Recorded of kill event from server
+}
+
+type EventKillRecord struct {
+	EventKillFromGame
+	//ServerGroup string `json:"server_group,required"` // ServerGroup is the logical grouping of events
+	ServerName string `json:"server_name,required"` // ServerName is the name of the server the kill occurred
+	GameName   string `json:"game_name,required"`   // GameName is the name of the physical game (e.g. "arma_reforger")
 }
