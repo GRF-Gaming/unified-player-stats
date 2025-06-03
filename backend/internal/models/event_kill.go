e485d6eecde6586c5519a74e382864926b75bd7d
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-06-03 23:29:24 +0800

    Updated schema to require killedEntityId

--- backend/internal/models/event_kill.go
+++ backend/internal/models/event_kill.go
@@ -5,7 +5,7 @@ import "time"
 type EventKillFromGame struct {
 	PlayerId       string    `json:"player_id,required" vd:"len($)>0"`   // PlayerId is the UID of the player designated by the server
 	PlayerName     string    `json:"player_name,required" vd:"len($)>0"` // PlayerName is the in-game name of the player
-	KilledEntityId string    `json:"killed_entity_id"`                   // KilledEntityId optional, only specifies the killed PLAYER
+	KilledEntityId string    `json:"killed_entity_id,required"`          // KilledEntityId only specifies the killed PLAYER; "" => AI
 	IsFriendly     bool      `json:"is_friendly,required"`               // IsFriendly specifies if the kill was a friendly kill
 	Time           time.Time `json:"time,required"`                      // Time Recorded of kill event from server
 }
