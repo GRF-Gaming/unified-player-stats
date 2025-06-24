2b610d823c2d18ee81f432ec8bc4e22b5fcf1fa2
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-06-24 23:39:11 +0800

    feat: added influxdb to docker compose file

--- backend/internal/utils/env_var/pro_kills_vars.go
+++ backend/internal/utils/env_var/pro_kills_vars.go
@@ -24,6 +24,14 @@ type ProKillsVars struct {
 	ProKillsRedisPassword      string `env:"PRO_KILLS_REDIS_PASSWORD" envDefault:""`
 	ProKillsRedisDbNumber      int    `env:"PRO_KILLS_REDIS_DB_NUMBER,required,notEmpty"`
 	ProKillsRedisMaxActiveConn int    `env:"PRO_KILLS_REDIS_MAX_ACTIVE_CONN" envDefault:"5"`
+
+	ProKillsInfluxDbAddr     string `env:"PRO_KILLS_INFLUXDB_ADDR"`
+	ProKillsInfluxDbPort     int    `env:"PRO_KILLS_INFLUXDB_PORT"`
+	ProKillsInfluxDbUsername string `env:"INFLUXDB_USERNAME"`
+	ProKillsInfluxDbPassword string `env:"INFLUXDB_PASSWORD"`
+	ProKillsInfluxDbOrg      string `env:"INFLUXDB_ORG"`
+	ProKillsInfluxDbBucket   string `env:"INFLUXDB_BUCKET"`
+	ProKillsInfluxDbToken    string `env:"INFLUXDB_ADMIN_TOKEN"`
 }
 
 func (p *ProKillsVars) validateAndPopulate() error {
