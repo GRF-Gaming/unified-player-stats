e4ce278a6107b5a912e60ef41d3408ad3c576e93
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-06-24 23:41:25 +0800

    feat: added event store
    Persists events into influxdbv2

--- backend/internal/processor_kills/processor.go
+++ backend/internal/processor_kills/processor.go
@@ -25,6 +25,7 @@ type Processor struct {
 	cancelFunc    context.CancelFunc
 	kafkaConsumer *kgo.Client
 	killStore     *store.KillStore
+	eventStore    *store.EventStore
 }
 
 func GetProcessor() *Processor {
@@ -39,8 +40,8 @@ func GetProcessor() *Processor {
 			e.ProKillsTopicKills,
 		)
 		if err != nil {
-			slog.Error("Unable to create kafka consumer client for kill processor")
-			log.Fatal("Unable to create Processor")
+			slog.Error("[GetProcessor] Unable to create kafka consumer client for kill processor")
+			log.Fatal("[GetProcessor] Unable to create Processor")
 		}
 
 		rClient, err := db.NewRedisClient(
@@ -51,9 +52,17 @@ func GetProcessor() *Processor {
 			e.ProKillsRedisMaxActiveConn,
 		)
 		if err != nil {
-			slog.Error("Unable to create redis conn client for kill processor")
-			log.Fatal("Unable to create Processor")
+			slog.Error("[GetProcessor] Unable to create redis conn client for kill processor")
+			log.Fatal("[GetProcessor] Unable to create Processor")
 		}
+
+		eventStore := store.NewEventStore(
+			e.ProKillsInfluxDbAddr,
+			e.ProKillsInfluxDbPort,
+			e.ProKillsInfluxDbToken,
+			e.ProKillsInfluxDbOrg,
+			e.ProKillsInfluxDbBucket,
+		)
 
 		ctx, cancel := context.WithCancel(context.Background())
 
@@ -62,6 +71,7 @@ func GetProcessor() *Processor {
 			cancelFunc:    cancel,
 			kafkaConsumer: consumer,
 			killStore:     store.NewKillStore(rClient),
+			eventStore:    eventStore,
 		}
 	})
 
@@ -79,7 +89,7 @@ func (p *Processor) Spin() {
 		default:
 			fetches := p.kafkaConsumer.PollFetches(p.ctx)
 			if fetches.IsClientClosed() {
-				slog.Error("Kafka client has been closed, exiting")
+				slog.Error("[Processor][Spin] Kafka client has been closed, exiting")
 				return
 			}
 
@@ -130,15 +140,28 @@ func (p *Processor) ProcessKillRecord(r *models.EventKillRecord) error {
 
 	// Handle updating of KillStore
 	if err := p.killStore.RecordKill(p.ctx, r.GameName, r.ServerName, r.Time, r.PlayerId); err != nil {
-		return err
+		slog.Error("[Processor][ProcessKillRecord] Unable to record kill in kill store", "err", err)
 	}
+	if err := p.eventStore.RecordKill(p.ctx, r.GameName, r.ServerName, r.Time, r.PlayerId); err != nil {
+		slog.Error("[Processor][ProcessKillRecord] Unable to record kill in event store", "err", err)
+	}
+
+	// Handle events related to friendly kills
 	if r.IsFriendly {
 		if err := p.killStore.RecordFriendlyKill(p.ctx, r.GameName, r.ServerName, r.Time, r.PlayerId); err != nil {
-			return err
+			slog.Error("[Processor][ProcessKillRecord] Unable to record friendly fire kill in kill store", "err", err)
+		}
+		if err := p.eventStore.RecordFriendlyKillWithVictim(p.ctx, r.GameName, r.ServerName, r.Time, r.PlayerId, r.KilledEntityId); err != nil {
+			slog.Error("[Processor][ProcessKillRecord] Unable to record friendly fire kill in event store", "err", err)
 		}
 	}
+
+	// Handle deaths of a player
 	if err := p.killStore.RecordDeath(p.ctx, r.GameName, r.ServerName, r.Time, r.KilledEntityId); err != nil {
-		return err
+		slog.Error("[Processor][ProcessKillRecord] Unable to record player death in kill store", "err", err)
+	}
+	if err := p.eventStore.RecordDeath(p.ctx, r.GameName, r.ServerName, r.Time, r.KilledEntityId); err != nil {
+		slog.Error("[Processor][ProcessKillRecord] Unable to record player death in event store", "err", err)
 	}
 
 	return nil
---
+++ backend/internal/store/event_store.go
@@ -0,0 +1,120 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
+	"github.com/influxdata/influxdb-client-go/v2/api/write"
+	"log/slog"
+	"time"
+)
+
+const (
+	pointRecordKill         string = "kill"
+	pointRecordFriendlyKill string = "ff_kill"
+	pointRecordDeath        string = "death"
+)
+
+type EventStore struct {
+	client   influxdb2.Client
+	writeApi api.WriteAPI
+}
+
+func NewEventStore(
+	addr string,
+	port int,
+	token string,
+	org string,
+	bucket string,
+) *EventStore {
+
+	slog.Info("[NewEventStore] Created a new event store")
+
+	client := influxdb2.NewClient(fmt.Sprintf("%s:%d", addr, port), token)
+	wApi := client.WriteAPI(org, bucket)
+
+	return &EventStore{
+		client:   client,
+		writeApi: wApi,
+	}
+}
+
+func (e *EventStore) CleanUp() {
+	slog.Info("[EventStore][CleanUp] Closing influxdb client")
+	e.writeApi.Flush()
+	e.client.Close()
+}
+
+func (e *EventStore) RecordKill(
+	ctx context.Context,
+	gameName string,
+	serverName string,
+	time time.Time,
+	playerId string,
+) error {
+
+	point := write.NewPoint(
+		pointRecordKill,
+		map[string]string{
+			"game_name":   gameName,
+			"server_name": serverName,
+			"player_id":   playerId,
+		},
+		map[string]interface{}{"count": 1},
+		time,
+	)
+
+	e.writeApi.WritePoint(point)
+
+	return nil
+}
+
+func (e *EventStore) RecordFriendlyKillWithVictim(ctx context.Context,
+	gameName string,
+	serverName string,
+	time time.Time,
+	playerId string,
+	victimPlayerId string,
+) error {
+
+	point := write.NewPoint(
+		pointRecordFriendlyKill,
+		map[string]string{
+			"game_name":        gameName,
+			"server_name":      serverName,
+			"player_id":        playerId,
+			"victim_player_id": victimPlayerId,
+		},
+		map[string]interface{}{"count": 1},
+		time,
+	)
+
+	e.writeApi.WritePoint(point)
+
+	return nil
+}
+
+func (e *EventStore) RecordDeath(
+	ctx context.Context,
+	gameName string,
+	serverName string,
+	time time.Time,
+	playerId string,
+) error {
+
+	point := write.NewPoint(
+		pointRecordDeath,
+		map[string]string{
+			"game_name":   gameName,
+			"server_name": serverName,
+			"player_id":   playerId,
+		},
+		map[string]interface{}{"count": 1},
+		time,
+	)
+
+	e.writeApi.WritePoint(point)
+
+	return nil
+}
